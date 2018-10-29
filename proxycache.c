b83c39cc140d92a01e6fa5f1091527b18c91e485
Conor Cunningham <ccunnin5@binghamton.edu>
2018-10-29 19:53:46 -0400

    Add to cache function

--- proxycache.c
+++ proxycache.c
@@ -61,15 +61,90 @@ void addToCache(char *url, char *object, int object_size) {
     printf("Available memeory in cache: %d\n", (MAX_CACHE_SIZE - cache_size));
     printf("Is space available: %d\n", (num_nodes != MAX_LRU_ELEMENTS));
 
-    cache[num_nodes]->buf = object;
-    cache[num_nodes]->time = time(NULL);
-    strcpy(cache[num_nodes]->url, url);
-    cache[num_nodes]->buf_len = object_size;
+    /* Check if the object fits in the cache */
+    if (object_size >= MAX_OBJECT_SIZE) {
+    	fprintf(stderr, "Could not add cache entry due to the object size\n");
+    	return;
+    }
+
+    /* Check if cache is empty */
+    if (num_nodes == 0) {
+	    cache[num_nodes]->buf = object;
+	    cache[num_nodes]->time = time(NULL);
+	    strcpy(cache[num_nodes]->url, url);
+	    cache[num_nodes]->buf_len = object_size;
+
+	    /* Update counts */
+	    num_nodes++;
+	    cache_size += object_size;
+
+		return;
+    }
+
+    /* Check if there is room in the cache for the object */
+    int bytes_available = (MAX_CACHE_SIZE - cache_size);
+
+    /* Get oldest node index */
+	int oldest_node_index = num_nodes - 1;
+
+    /* Check if the object fits in the cache / evict nodes to make space */
+	while ( (bytes_available < object_size) && oldest_node_index >= 0) {
+		/* Evict each node */
+		Node *evicted_node = cache[oldest_node_index];
+
+		/* Free the stored buffer */
+		free(evicted_node->buf);
+
+		/* Updated the cache sizes */
+		cache_size -= evicted_node->buf_len;
+		num_nodes--;
+
+		/* Set node to default values */
+		evicted_node->buf = NULL;
+		evicted_node->buf_len = 0;
+		evicted_node->time = 0;
+		memset(evicted_node->url, 0, MAX_URL_CHARS);
+
+		/* Check next oldest node */
+		oldest_node_index--;
+	}
+
+    /* Check if the object fits in the cache after evictions */
+    if (bytes_available < object_size) {
+    	fprintf(stderr, "Could not add cache entry due to lack of memory\n");
+    	return;
+    }
+
+    /* Check if the cache is full */
+    if (num_nodes == MAX_LRU_ELEMENTS) {
+		/* Evict the oldest node node */
+		Node *evicted_node = cache[oldest_node_index];
+
+		/* Free the stored buffer */
+		free(evicted_node->buf);
+
+		/* Updated the cache sizes */
+		cache_size -= evicted_node->buf_len;
+		num_nodes--;
+
+		/* Set node to default values */
+		evicted_node->buf = NULL;
+		evicted_node->buf_len = 0;
+		evicted_node->time = 0;
+		memset(evicted_node->url, 0, MAX_URL_CHARS);
+    } 
+
+    /* Set the new node values */
+    cache[oldest_node_index]->buf = object;
+    cache[oldest_node_index]->time = time(NULL);
+    strcpy(cache[oldest_node_index]->url, url);
+    cache[oldest_node_index]->buf_len = object_size;
 
     /* Update counts */
     num_nodes++;
     cache_size += object_size;
 
+    /* Sort the cache */
    	qsort(cache, MAX_LRU_ELEMENTS, sizeof(Node*), comp_time);
 
 }
@@ -85,7 +160,6 @@ void getFromCache(char *url, char *object, int *object_size) {
 
 	/* Search table for url */
     for (int i = 0; i < MAX_LRU_ELEMENTS; i++) {
-		printf("url: %s\n", cache[i]->url);
     	if (strcmp(cache[i]->url, url) != 0) {
 			node = cache[i];
 			break;
